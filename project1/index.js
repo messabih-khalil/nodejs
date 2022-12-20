e6efad602a74096f75dd0d715a9984eb39dd48bb
messabih-khalil <msbih.khalil@gmail.com>
2022-12-21 00:13:30 +0100

    project 1 nodejs

--- project1/index.js
+++ project1/index.js
@@ -1,64 +1,8 @@
-const fs = require("fs");
+
 const http = require("http");
 const url = require("url");
 
-const getData = () => {
-  const productData = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
-  return productData;
-};
-
-const serilizeData = () => {
-  const dataObj = JSON.parse(getData());
-  return dataObj;
-};
-
-const generatePosts = () => {
-  let htmlBody = ``;
-  serilizeData().forEach(e => {
-    htmlBody += `
-    <a href="/product?id=${e.id}">
-    <ol>
-    <li>User Id : ${e.userId}</li>
-    <li>Id : ${e.id}</li>
-    <li>
-      Title : ${e.id}
-    </li>
-    <li>
-      body : ${e.body}
-    </li>
-  </ol>
-    </a>
-  <hr/>
-    `;
-  });
-
-  return htmlBody;
-};
-
-const getPostDetails = id => {
-  let htmlBody;
-  serilizeData().forEach(e => {
-    if (parseInt(e.id) == parseInt(id)) {
-      htmlBody = `
-      <a href="/product?id=${e.id}">
-      <ol>
-      <li>User Id : ${e.userId}</li>
-      <li>Id : ${e.id}</li>
-      <li>
-        Title : ${e.id}
-      </li>
-      <li>
-        body : ${e.body}
-      </li>
-    </ol>
-      </a>
-    <hr/>
-      `;
-    }
-  });
-
-  return htmlBody;
-};
+const { generatePosts, getPostDetails } = require("./modules/module");
 
 // create server
 
